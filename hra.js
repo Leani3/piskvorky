39549da2183de2f565ff519691e4aafc3d51d2f7
Líza P <liza.povkh@gmail.com>
2023-10-25 22:55:53 +0200

    piskvorky 5

--- hra.js
+++ hra.js
@@ -1,7 +1,6 @@
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
 let currentPlayer = 'circle';
-
 const buttons = document.querySelectorAll('.game-board button');
 
 const playerMove = (event) => {
@@ -18,7 +17,10 @@ const playerMove = (event) => {
   }
 
   event.target.disabled = true;
+  gameCheck();
+};
 
+const gameCheck = async () => {
   let herniPole = [];
 
   buttons.forEach((button) => {
@@ -45,6 +47,40 @@ const playerMove = (event) => {
       window.location.reload();
     };
     setTimeout(tieIs, 300);
+  } else if (vitez === null && currentPlayer === 'cross') {
+    buttons.forEach((button) => {
+      button.disabled = true;
+    });
+    const response = await fetch(
+      'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
+      {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          board: herniPole,
+          player: 'x',
+        }),
+      },
+    );
+
+    const data = await response.json();
+    const { x, y } = data.position;
+    const field = buttons[x + y * 10];
+    field.disabled = false;
+    field.click();
+  } else {
+    buttons.forEach((button) => {
+      if (
+        button.classList.contains('board__field--circle') ||
+        button.classList.contains('board__field--cross')
+      ) {
+        button.disabled = true;
+      } else {
+        button.disabled = false;
+      }
+    });
   }
 };
 
